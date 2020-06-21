2716de5a02ff2be91300d71bbd0ed93ebddd9d44
vegirevathi <vegirevathi@gmail.com>
2020-06-21 13:28:26 +0530

    ADDED:AnalyzeFloatMax method to find maximum of three floats

--- src/test/java/com/bridgelabz/FindingMaximumTest.java
+++ src/test/java/com/bridgelabz/FindingMaximumTest.java
@@ -25,7 +25,29 @@ public class FindingMaximumTest {
         Integer max = findingMaximum.analyzeMaximum( 10,20,30 );
         Assert.assertEquals(30,(int)max);
     }
+
+    @Test
+    public void maximumFloat_at_firstPosition() {
+        FindingMaximum findingMaximum = new FindingMaximum();
+        Double max = findingMaximum.analyzeFloatMax(4.2,2.5,3.8);
+        Assert.assertEquals((Double) 4.2,(Double)max);
+    }
+
+    @Test
+    public void maximumFloat_at_secondPosition() {
+        FindingMaximum findingMaximum = new FindingMaximum();
+        Double max = findingMaximum.analyzeFloatMax(4.2,5.2,3.8);
+        Assert.assertEquals((Double) 5.2,(Double)max);
+    }
+
+    @Test
+    public void maximumFloat_at_thirdPosition() {
+        FindingMaximum findingMaximum = new FindingMaximum();
+        Double max = findingMaximum.analyzeFloatMax(4.2,2.5,7.8);
+        Assert.assertEquals((Double) 7.8,(Double)max);
+    }
 }
+
 
 
 
